c34cfd7199a852c43f32a7d973b0cee36f68b6b5
Safwan Muntasir <smuntasi@calpoly.edu>
2024-05-15 10:58:28 -0700

    draw points

--- StatPanel.java
+++ StatPanel.java
@@ -1,8 +1,8 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
+
 public class StatPanel extends JPanel {
     private List<Point> points;
 
@@ -12,8 +12,25 @@ public class StatPanel extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-        //draw 5 points
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
 
-        //draw lines connecting points
+        // Draw points
+        g2d.setColor(Color.BLUE);
+        for (Point point : points) {
+            int x = (int) point.getX();
+            int y = (int) point.getY();
+            g2d.fillOval(x - 5, y - 5, 10, 10);
+        }
+
+        int middleIndex = points.size() / 2;
+        Point middlePoint = points.get(middleIndex);
+
+        // Draw lines connecting points to the middle point
+        g2d.setColor(Color.BLUE);
+        for (Point point : points) {
+            g2d.drawLine((int) point.getX(), (int) point.getY(), (int) middlePoint.getX(), (int) middlePoint.getY());
+        }
     }
+
 }
