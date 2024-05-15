34ff9f73036a3ceeb8f06f14b03348f65929eef8
ethanhandelman <ehandelm@calpoly.edu>
2024-05-15 15:29:32 -0700

    Added weather class

---
+++ src/main/java/com/threezeronine/Weather.java
@@ -0,0 +1,12 @@
+package com.threezeronine;
+
+/*
+    Created by Team 3:
+        Ethan Handelman
+        Luis Rodriguez
+        Devin Hadley
+        Miro Haapalainen
+ */
+public class Weather {
+
+}
